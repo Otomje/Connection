1a1b4c2938546a72a32e075c697d8773029db13c
Otomje <d8315461@gmail.com>
2025-08-09 12:49:37 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
+function updateClock() {
+  const clockEl = document.getElementById("clockText");
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  clockEl.textContent = `${hours}:${minutes}`;
+}
 
+window.addEventListener("load", () => {
+  const clockEl = document.getElementById("clockText");
+  clockEl.textContent = "· · ·";
+
+  setTimeout(() => {
+    updateClock();
+    setInterval(updateClock, 1000);
+  }, 500);
+
+  setTimeout(updateStatus, 500);
+});
+
+function updateStatus() {
+  const textEl = document.getElementById("statusText");
+  const phonEl = document.querySelector(".phon");
+
+  if (navigator.onLine) {
+    textEl.textContent = "З'єднання присутнє";
+    phonEl.style.background = "var(--ess)";
+  } else {
+    textEl.textContent = "З'єднання перервано";
+    phonEl.style.background = "var(--dnt)";
+  }
+}
+
+window.addEventListener("online", updateStatus);
+window.addEventListener("offline", updateStatus);
+
+document.querySelector(".phon").addEventListener("click", () => {
+  const docEl = document.documentElement;
+
+  if (!document.fullscreenElement) {
+    docEl.requestFullscreen().catch(() => {});
+  } else {
+    document.exitFullscreen();
+  }
+});
